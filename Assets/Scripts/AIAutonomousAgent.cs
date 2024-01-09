afcdc3230b1c241c01b17f4566a2a148ff643d98
KWadas <kwadas@student.neumont.edu>
2024-01-09 14:22:11 -0700

    Added AI scripts

---
+++ Assets/Scripts/AIAutonomousAgent.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIAutonomousAgent : AIAgent
+{
+    public AIPerception perception = null;
+
+    private void Update()
+    {
+        //
+    }
+}
