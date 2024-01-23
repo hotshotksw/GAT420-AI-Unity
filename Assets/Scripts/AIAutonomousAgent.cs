2f5ce3a0842b5302e2e1177d1cfa3b557b50dd0f
KWadas <kwadas@student.neumont.edu>
2024-01-23 13:58:38 -0700

    Added pathing

--- Assets/Scripts/AIAutonomousAgent.cs
+++ Assets/Scripts/AIAutonomousAgent.cs
@@ -46,12 +46,27 @@ public class AIAutonomousAgent : AIAgent
         }
 
         // obstacle avoidance
+        if (obstaclePerception != null)
         {
-            var gameObjects = obstaclePerception.GetGameObjects();
+            if (((AISphereCastPerception)obstaclePerception).CheckDirection(Vector3.forward))
+            {
+                Vector3 open = Vector3.zero;
+                if (((AISphereCastPerception)obstaclePerception).GetOpenDirection(ref open))
+                {
+                    movement.ApplyForce(GetSteeringForce(open) * 5);
+                }
+
+            }
         }
 
+        Vector3 acceleration = movement.Acceleration;
+        acceleration.y = 0;
+        movement.Acceleration = acceleration;
+        //movement.Acceleration = new Vector3(movement.Acceleration.x, 0, movement.Acceleration.z); // Takes more space for new vector, needs optimization
+
+
         // wrap position in world
-		transform.position = Utilities.Wrap(transform.position, new Vector3(-8, -1, -8), new Vector3(8, 10, 8));
+        //transform.position = Utilities.Wrap(transform.position, new Vector3(-22, -1, -22), new Vector3(22, 10, 22));
     }
 
     private Vector3 Seek(GameObject target)
--- Assets/Scripts/AIRaycastPerception.cs
+++ Assets/Scripts/AIRaycastPerception.cs
@@ -15,9 +15,10 @@ public class AIRaycastPerception : AIPerception
 		foreach (Vector3 direction in directions)
 		{
 			Ray ray = new Ray(transform.position, transform.rotation * direction);
-			Debug.DrawRay(ray.origin, ray.direction * distance, Color.red);
+
 			if (Physics.Raycast(ray, out RaycastHit raycastHit, distance))
 			{
+				Debug.DrawRay(ray.origin, ray.direction * raycastHit.distance, Color.red);
 				// check if collision is self, skip if so
 				if (raycastHit.collider.gameObject == gameObject) continue;
 				if (tagName == "" || raycastHit.collider.CompareTag(tagName))
@@ -25,11 +26,45 @@ public class AIRaycastPerception : AIPerception
 					result.Add(raycastHit.collider.gameObject);
 				}
 			}
+			else
+			{
+				Debug.DrawRay(ray.origin, ray.direction * distance, Color.green);
+			}
 		}
 
 		// remove duplicates
 		result = result.Distinct().ToList();
 
 		return result.ToArray();
+	}
+
+	public bool GetOpenDirection(ref Vector3 openDirection)
+	{
+		Vector3[] directions = Utilities.GetDirectionsInCircle(numRaycast, maxAngle);
+		foreach (var direction in directions)
+		{
+			// cast ray from transform position towards direction (use game object orientation)
+			Ray ray = new Ray(transform.position, transform.rotation * direction);
+			// if there is NO raycast hit then that is an open direction
+			if (!Physics.Raycast(ray, out RaycastHit raycastHit, distance, layerMask))
+			{
+				Debug.DrawRay(ray.origin, ray.direction * distance, Color.green);
+				// set open direction
+				openDirection = ray.direction;
+				return true;
+			}
+		}
+
+		// no open direction
+		return false;
+	}
+
+	public bool CheckDirection(Vector3 direction)
+	{
+		// create ray in direction (use game object orientation)
+		Ray ray = new Ray(transform.position, transform.rotation * direction);
+		// check ray cast
+		return Physics.Raycast(ray, distance, layerMask);
+
 	}
 }
